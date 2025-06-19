cc45734254875bcedf9717d1471f1da67f382abd
Maksym Buslenko <maksbusl@gmail.com>
2025-06-19 17:38:12 +0300

    fix return

--- app/main.py
+++ app/main.py
@@ -3,13 +3,13 @@ def copy_file(
 ) -> None:
     command_elem = command.split()
     if len(command_elem) < 3:
-        return None
+        return
     if command_elem[0] == "invalid_command":
-        return None
+        return
     if "non_existing_file.txt" == command_elem[1]:
-        return None
+        return
     if command_elem[1] == command_elem[2]:
-        return None
+        return
     with (open(command_elem[1], "r") as file_in,
           open(command_elem[2], "w") as file_out):
         for line in file_in:
