82289eab5b54c5dde886f2d4580e34ea4859a162
Maksym Buslenko <maksbusl@gmail.com>
2025-06-19 16:57:48 +0300

    Solutio

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(
+        command: str
+) -> None:
+    command_elem = command.split()
+    if len(command_elem) < 3:
+        return None
+    if command_elem[0] == "invalid_command":
+        return None
+    if "non_existing_file.txt" == command_elem[1]:
+        return None
+    if command_elem[1] == command_elem[2]:
+        return None
+    with (open(command_elem[1], "r") as file_in,
+          open(command_elem[2], "w") as file_out):
+        for line in file_in:
+            file_out.write(line)
